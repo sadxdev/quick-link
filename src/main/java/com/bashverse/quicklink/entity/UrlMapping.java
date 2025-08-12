a35ab0dfb13952c2485cd97b52c0b0272d48e761
sadxdev <sadxdev@gmail.com>
2025-08-13 00:00:24 +0530

    Included entity

---
+++ src/main/java/com/bashverse/quicklink/entity/UrlMapping.java
@@ -0,0 +1,36 @@
+package com.bashverse.quicklink.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String shortCode;
+
+    @Builder.Default
+    private Long hitCount = 0L;
+
+    private String longUrl;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() { this.createdAt = LocalDateTime.now(); }
+
+    @PreUpdate
+    protected void onUpdate() { this.updatedAt = LocalDateTime.now(); }
+}
