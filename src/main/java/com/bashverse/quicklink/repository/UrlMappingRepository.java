d2e7fb200a74500384da55e2ce5146daae683663
sadxdev <sadxdev@gmail.com>
2025-08-13 00:02:26 +0530

    included repository

---
+++ src/main/java/com/bashverse/quicklink/repository/UrlMappingRepository.java
@@ -0,0 +1,10 @@
+package com.bashverse.quicklink.repository;
+
+import com.bashverse.quicklink.entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    Optional<UrlMapping> findByShortCode(String shortCode);
+}
